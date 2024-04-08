25cb7af4e0ec45233e48d293234fd74b6be15367
anwarsayyad <anwarsayyad2631@gmail.com>
2024-04-08 09:58:00 +0000

    trying to use docker

--- My_apps/to_do_list/api/serializer.py
+++ My_apps/to_do_list/api/serializer.py
@@ -1,21 +1,28 @@
 from rest_framework import serializers
 from to_do_list.models import Task,Project,Comments,TeamMember
+from django.contrib.auth.models import User
 
-class TaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Task
-        fields = '__all__'
-    
-class ProjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Project
-        fields = '__all__'
+
 
 class TeamMemeberSerializers(serializers.ModelSerializer):
     class Meta:
         model = TeamMember
         fields = '__all__'
         
+class TaskSerializer(serializers.ModelSerializer):
+    project = serializers.PrimaryKeyRelatedField(queryset = Project.objects.all())
+    created_by = serializers.PrimaryKeyRelatedField(queryset = User.objects.all())
+    assigned_to = TeamMemeberSerializers()
+    class Meta:
+        model = Task
+        fields = '__all__'
+    
+class ProjectSerializer(serializers.ModelSerializer):
+    created_by = serializers.StringRelatedField()
+    class Meta:
+        model = Project
+        fields = '__all__'
+
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comments
